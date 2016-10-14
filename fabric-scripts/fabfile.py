e01de4177efe5f7d3531a90280147f8d30814383
Geoffrey Cline <gdcwd5@mail.mst.edu>
2016-10-14 15:40:55 -0500

    add rundgi

--- fabric-scripts/fabfile.py
+++ fabric-scripts/fabfile.py
@@ -1,14 +1,46 @@
 from fabric.api import *
 
-env.key_filename = ["/home/scj7t4/.ssh/id_supercluster"]
+env.key_filename = ["/home/geoffreyc/.ssh/id_rsa"]
 env.warn_only = False
 #env.hosts = ['r-facts4.device.mst.edu',
 #             'r-facts5.device.mst.edu','r-facts6.device.mst.edu']
 #env.hosts += ['r99ff.managed.mst.edu','r12ff.managed.mst.edu',
 #              'r01ff.managed.mst.edu','fil4.managed.mst.edu']
-#env.user = 'root'
+env.user = 'geoffcline'
 
-env.hosts = ['r02scj7t4.device.mst.edu:%d' % x for x in range(2022,2028)]
+freedmhosts = ['r02scj7t4.device.mst.edu:%d' % x for x in range(2022,2027)]
+
+brokerhost = freedmhosts[0]
+
+brokeradd = '127.0.0.1'
+
+env.roledefs = {
+    'sst':  freedmhosts[1],
+    'desd': freedmhosts[2]
+}
+
+env.hosts = list(freedmhosts)
+
+
+@roles('sst')
+def runsst():
+    with cd('MQTT-Device'):
+        run('python MQTT_server.py -d SST_1 -b %s' % brokeradd)
+
+@roles('desd')
+def rundesd():
+    with cd('MQTT-Device'):
+        run('python MQTT_server.py -d DESD_1 -b %s' % brokeradd)
+
+@hosts(brokerhost)
+def mosquitto():
+    run('mosquitto')
+    run('./PosixBroker')
+
+def runmqtt():
+    mosquitto()
+    runsst()
+    rundesd()
 
 def host_type():
     run('uname -s')
@@ -40,7 +72,11 @@ def put_key_authorization(keyfile='~/.ssh/id_rsa.pub'):
     run('mkdir -p .ssh')
     put(keyfile,'tmp_key')
     run('cat tmp_key >> ~/.ssh/authorized_keys')
-    run('rm tmp_key') 
+    run('rm tmp_key')
+
+def clonemqtt():
+    run('rm -rf MQTT-Device')
+    clone_from_host('https://github.com/FREEDM-DGI/MQTT-Device.git')
 
 def clone_from_host(gitpath):
     run('git clone %s' % gitpath)
@@ -77,6 +113,10 @@ def pull_checkout(branch='master'):
 def build(options=""):
     with cd('~/FREEDM/Broker'):
         run("cmake . %s" % options)
-    with cd('~/FREEDM/Broker/src'):
-        run("make")
+    with cd('~/FREEDM/Broker'):
+        run("make -j2")
+
+def rundgi():
+    with cd('~/FREEDM/Broker'):
+        run("./PosixBroker")
 
